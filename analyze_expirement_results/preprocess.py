9ce1ef3d38a3a45a773b86bd8fad56b90c410143
Yair Shimony <yairshsh@gmail.com>
2024-12-14 16:41:59 +0200

    add acclimation days

--- analyze_expirement_results/preprocess.py
+++ analyze_expirement_results/preprocess.py
@@ -7,6 +7,11 @@ from utils import (variable_equals_value, setup_logger, get_column_groups_sorted
 
 def permanent_preprocess_data():
     outputs_preprocess_dir = DATA_DIR / 'preprocess'
+    data_processed_path = outputs_preprocess_dir / 'Final_Data_Voyages_Processed.csv'
+
+    if os.path.exists(data_processed_path):
+        return pd.read_csv(data_processed_path)
+
     os.makedirs(outputs_preprocess_dir, exist_ok=True)
     logger = setup_logger(outputs_preprocess_dir / 'preprocess.log', 'PREPROCESS')
 
@@ -21,11 +26,11 @@ def permanent_preprocess_data():
 
     replace_lived_indicators(df, lived_columns)
     add_dying_day(df, lived_columns, temperature_columns, salinity_columns)
+    add_acclimation_days(df)
 
-    data_processed_path = outputs_preprocess_dir / 'Final_Data_Voyages_Processed.csv'
     df.to_csv(data_processed_path, index=False)
 
-    logger.info(f"There are {len(df)} routes in the dataset. {df['dying_day'].notna().sum()} of them ended with death,"
+    logger.info(f"There are {len(df)} routes in the dataset. {df['dying_day'].notna().sum()} of them ended with death, "
                 f"and {df['dying_day'].isna().sum()} did not. Preprocessed data saved to {data_processed_path}")
 
     return df
@@ -121,3 +126,18 @@ def remove_suspected_routes_parts(df):
         for col in lived_columns + temperature_columns + salinity_columns:
             if int(col.split()[1]) >= row['Suspected from time point']:
                 df.loc[idx, col] = pd.NA
+
+
+def add_acclimation_days(df):
+    # Add columns for the 2 days before Day 0 because there were total 3 days of acclimation
+    temp_0_col_index = df.columns.get_loc('Temp 0')
+    df.insert(temp_0_col_index, 'Temp -1', df['Temp 0'])
+    df.insert(temp_0_col_index, 'Temp -2', df['Temp 0'])
+
+    salinity_0_col_index = df.columns.get_loc('Salinity 0')
+    df.insert(salinity_0_col_index, 'Salinity -1', df['Salinity 0'])
+    df.insert(salinity_0_col_index, 'Salinity -2', df['Salinity 0'])
+
+    lived_0_col_index = df.columns.get_loc('Lived 0')
+    df.insert(lived_0_col_index, 'Lived -1', df['Lived 0'])
+    df.insert(lived_0_col_index, 'Lived -2', df['Lived 0'])
